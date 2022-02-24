b41fe6cd8e2e69970030830d497ac8bdd9604cac
Juli Arnalot <juli.arnalot@xing.com>
2022-02-24 15:49:30 +0100

    wip

--- backend/router/game.ts
+++ backend/router/game.ts
@@ -3,23 +3,45 @@ import { z } from "zod";
 import { prisma } from "backend/utils/prisma";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
+const getLatestRatingByPlayer = async (playerId: bigint) => {
+  const ratingPlayers = await prisma.ratings_history.findMany({
+    select: {
+      player_id: true,
+      rating: true,
+      game_result_id: true,
+      created_at: true
+    },
+    take: 2,
+    where: {
+      player_id: playerId,
+    },
+    orderBy: [
+      {
+        created_at: "desc",
+      },
+    ],
+  });
+  console.log("ratingPlayers666", ratingPlayers, playerId)
+  return ratingPlayers;
+};
 export const gameRouter = trpc
   .router()
   .query("getAll", {
     async resolve({ input }) {
       const games = await prisma.game_results.findMany({
         include: {
-          ratings_history: true,
           users_game_results_usa_player_idTousers: {
             select: {
               first_name: true,
               last_name: true,
+              country_id: true
             },
           },
           users_game_results_ussr_player_idTousers: {
             select: {
               first_name: true,
               last_name: true,
+              country_id: true
             },
           },
         },
@@ -30,21 +52,33 @@ export const gameRouter = trpc
           },
         ],
       });
-      const gamesNormalized = games.map((game) => ({
-        ...game,
-        reportedAt: game.reported_at,
-        endMode: game.end_mode,
-        endTurn: game.end_turn,
-        usaPlayer:
-          game.users_game_results_usa_player_idTousers.first_name +
-          " " +
-          game.users_game_results_usa_player_idTousers.last_name,
-        urssPlayer:
-          game.users_game_results_ussr_player_idTousers.first_name +
-          " " +
-          game.users_game_results_ussr_player_idTousers.last_name,
-        gameType: game.game_type,
-        gameWinner: game.game_winner,
+
+      const gamesNormalized = await Promise.all(games.map(async (game) => {
+        const usaPlayerRatings = await getLatestRatingByPlayer(game.usa_player_id);
+        const ussrPlayerRatings = await getLatestRatingByPlayer(game.ussr_player_id);
+        return {
+          // ...game,
+          created_at: game.created_at,
+          endMode: game.end_mode,
+          endTurn: game.end_turn,
+          usaPlayerId: game.usa_player_id,
+          ussrPlayerId: game.ussr_player_id,
+          usaCountryId: game.users_game_results_usa_player_idTousers.country_id,
+          ussrCountryId: game.users_game_results_ussr_player_idTousers.country_id,
+          usaPlayer:
+            game.users_game_results_usa_player_idTousers.first_name +
+            " " +
+            game.users_game_results_usa_player_idTousers.last_name,
+          ussrPlayer:
+            game.users_game_results_ussr_player_idTousers.first_name +
+            " " +
+            game.users_game_results_ussr_player_idTousers.last_name,
+          gameType: game.game_type,
+          videoURL: game.video1,
+          gameWinner: game.game_winner,
+          ratingsUSA: usaPlayerRatings,
+          ratingsUSSR: ussrPlayerRatings,
+        };
       }));
       const gameParsed = JSON.stringify(gamesNormalized, (key, value) =>
         typeof value === "bigint" ? value.toString() : value
