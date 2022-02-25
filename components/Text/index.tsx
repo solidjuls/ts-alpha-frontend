6739aa3ed58999f4fb073a690a02f776bc255b75
Juli Arnalot <juli.arnalot@xing.com>
2022-02-25 18:12:15 +0100

    added open sans font

--- components/Text/index.tsx
+++ components/Text/index.tsx
@@ -3,10 +3,11 @@ import { styled } from "stitches.config";
 const Text = styled("p", {
   fontWeight: "0",
   margin: '4px',
+  fontSize: '12px',
   variants: {
     strong: {
       bold: {
-        fontWeight: "600",
+        fontWeight: "700",
       },
     },
   },
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import { getCssText } from '../stitches.config';
+import React from "react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { getCssText } from "../stitches.config";
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+          <style
+            id="stitches"
+            dangerouslySetInnerHTML={{ __html: getCssText() }}
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="crossorigin"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@0;700&display=swap"
+            rel="stylesheet"
+          ></link>
         </Head>
         <body>
           <Main />
