169dac9f7e453a627965d95e030a3fb8b65d0268
Juli Arnalot <juli.arnalot@xing.com>
2022-02-25 09:23:23 +0100

    wip

---
+++ components/Homepage/index.jsx
@@ -0,0 +1,7 @@
+import styled from "stitches.config"
+import { Box } from "components/"
+const Homepage = () => {
+  return <div>Homepage</div>;
+};
+
+export default Homepage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,18 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useSession } from "next-auth/react";
 import { trpc } from "utils/trpc";
+import Homepage from "components/Homepage";
 
 const getResultDisplay = ({ game }: { game: any }) => {
-  return `${game.usaPlayer} (USA)  ${game.ratings_history.find((item:any) => item.player_id === game.usa_player_id).rating} vs   ${game.urssPlayer} (URSS) ${game.ratings_history.find((item:any) => item.player_id === game.ussr_player_id).rating} - Winner: ${game.gameWinner};`
+  return `${game.usaPlayer} (USA)  ${
+    game.ratings_history.find(
+      (item: any) => item.player_id === game.usa_player_id
+    ).rating
+  } vs   ${game.urssPlayer} (URSS) ${
+    game.ratings_history.find(
+      (item: any) => item.player_id === game.ussr_player_id
+    ).rating
+  } - Winner: ${game.gameWinner};`;
 };
 
 export default function Home() {
@@ -22,6 +31,7 @@ export default function Home() {
         <h1 className={styles.title}>HOME PAGE</h1>
         <section>Latest results:</section>
         <br />
+        <Homepage />
         {data?.map((game: any) => {
           return <div key={game.id}>{getResultDisplay({ game })}</div>;
         })}
