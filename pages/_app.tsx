42568363507009dee1f2ee5f0c9831827676e486
Juli Arnalot <juli.arnalot@gmail.com>
2024-08-10 09:25:33 +0200

    url from env file updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,9 +39,9 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    console.log("process.env.NEXT_PUBLIC_VERCEL_URL", process.env.NEXT_PUBLIC_URL);
-    const url = process.env.NEXT_PUBLIC_URL
-      ? `${process.env.NEXT_PUBLIC_URL}/api/trpc`
+    console.log("process.env.NEXT_PUBLIC_VERCEL_URL", process.env.VERCEL_URL);
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
       : "http://localhost:3000/api/trpc";
 
     return {
