185ac0e88d02aa6f997d2c9ccf3c4d9c766b755c
= <=>
2024-09-02 16:19:04 -0400

    removing promise;

--- pages/api/rating/index.ts
+++ pages/api/rating/index.ts
@@ -3,19 +3,17 @@ import { getAllPlayers } from "backend/controller/rating.controller";
 export default async function handler(req, res) {
   const { p, pso } = req.query;
   const players = await getAllPlayers(p, pso);
-  const playersWithRating = await Promise.all(
-    players.map(async (player) => {
-      return {
-        id: player.id.toString(),
-        name: player.first_name + " " + player.last_name,
-        rating: player.rating,
-        countryCode: player.tld_code,
-        lastActivity: player.last_login_at,
-        rank: parseInt(player.ranking),
-        totalPlayers: parseInt(player.total_players),
-      };
-    }),
-  );
+  const playersWithRating = players.map((player) => {
+    return {
+      id: player.id.toString(),
+      name: player.first_name + " " + player.last_name,
+      rating: player.rating,
+      countryCode: player.tld_code,
+      lastActivity: player.last_login_at,
+      rank: parseInt(player.ranking),
+      totalPlayers: parseInt(player.total_players),
+    };
+  });
 
   res.status(200).json(playersWithRating);
 }
