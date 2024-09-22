da6df683662a00ee948268987f4a4eebf2e21058
Juli Arnalot <juli.arnalot@gmail.com>
2024-09-22 09:44:18 +0200

    updated migration endpoint

---
+++ pages/api/game/submit-migration.ts
@@ -0,0 +1,55 @@
+import { submit } from "backend/controller/game.controller";
+import { authenticateJWT } from "pages/api/auth/middleware";
+const fs = require("fs");
+const axios = require("axios");
+const path = require("path");
+
+// Function to parse JSON file
+function parseJsonFile(filePath) {
+    try {
+      // Read the JSON file synchronously
+      const jsonData = fs.readFileSync(path.resolve(__dirname, filePath), "utf-8");
+  
+      // Parse the JSON data into an object
+      const parsedData = JSON.parse(jsonData);
+  
+      return parsedData;
+    } catch (error) {
+      console.error(`Error parsing JSON file: ${error}`);
+      return null;
+    }
+  }
+
+const parsedObject = parseJsonFile(
+    "../../../../../python/initial_data_seeding/games_json/first_20k_game_results_20240811.json",
+  );
+
+export default async function handler(req, res) {
+    try {
+        const arrayOfGames = req.body.data
+        // console.log("arrayOfGames", arrayOfGames)
+        for(let i = 0; i < parsedObject.length; i++) {
+            // console.log("arrayOfGames[i].usa_player_id.toString()", arrayOfGames[i].usa_player_id.toString())
+            const data = {
+                video1: undefined,
+                usaPlayerId: parsedObject[i].usa_player_id.toString(),
+                ussrPlayerId: parsedObject[i].ussr_player_id.toString(),
+                gameType: parsedObject[i].game_type,
+                gameWinner: parsedObject[i].game_winner.toString(),
+                endTurn: parsedObject[i].end_turn !== null ? parsedObject[i].end_turn : "",
+                endMode: parsedObject[i].end_mode || "",
+                gameDate: parsedObject[i].game_date,
+                gameCode: parsedObject[i].game_code,
+            }
+            await submit(data);
+        }
+    // console.log("newGameWithId", newGameWithId);
+    // const newGameWithIdParsed = JSON.stringify(newGameWithId, (key, value) =>
+    //     typeof value === "bigint" ? value.toString() : value,
+    // );
+
+    res.status(200).json("ok");
+    } catch {
+    res.status(500).json("Error submitting result");
+    }
+}
--- utils/migrationProcess.js
+++ utils/migrationProcess.js
@@ -25,38 +25,15 @@ async function main() {
       "../python/initial_data_seeding/games_json/first_20k_game_results_20240811.json",
     );
 
-    // parsedObject.slice(1).forEach((element) => {
-    for (let i = 0; i < parsedObject.length; i++) {
-      const ddd = await axios.post("http://localhost:3000/api/game/submit", {
+      await axios.post("http://localhost:3000/api/game/submit-migration", {
         method: "POST",
         headers: {
           "x-trpc-source": "client",
           "Content-Type": "application/json",
-        },
-        data: {
-          video1: undefined,
-          usaPlayerId: parsedObject[i].usa_player_id.toString(),
-          ussrPlayerId: parsedObject[i].ussr_player_id.toString(),
-          gameType: parsedObject[i].game_type,
-          gameWinner: parsedObject[i].game_winner.toString(),
-          endTurn: parsedObject[i].end_turn !== null ? parsedObject[i].end_turn : "",
-          endMode: parsedObject[i].end_mode || "",
-          gameDate: parsedObject[i].game_date,
-          gameCode: parsedObject[i].game_code,
-        },
-      });
-      console.log("done ", ddd.id);
-    }
-    // fetch('', {
-    //   method: "POST",
-    //   headers: {
-    //     'x-trpc-source': 'client',
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({})
-    // }).then(resp => resp.json()).then(resp => console.log('GameSubmit result:', resp));
-    //         console.log('GameSubmit result:', gameSubmit);
-    //  });
+        }
+      })
+    
+
   } catch (error) {
     console.error("Error occurred while calling tRPC endpoint:", error);
   }
