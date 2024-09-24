e94e05b2fdc4432ca73c71e72ad52bb4e820e130
Juli Arnalot <juli.arnalot@gmail.com>
2024-09-24 17:01:26 +0200

    closing the page before the feature flags (#56)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,7 +27,9 @@ function App({ Component, pageProps, name, id, email, role }: AppProps) {
         >
           <Theme>
             <Layout>
-              <Component {...pageProps} />
+              {/* <Component {...pageProps} /> */}
+              <h1>Please report your game <a href="https://docs.google.com/forms/d/e/1FAIpQLSc-IUEJSELdj2QgyEZ2V8iZB0hrGqnueFdWmpgrXaq3k0_TXg/viewform?pli=1" target="_blank" rel="noreferrer">here</a></h1>
+              <h2>We are sorry for the troubles! We will be back real soon!!</h2>
             </Layout>
           </Theme>
         </ThemeProvider>
