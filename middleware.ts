37df871b4bb67b9b8cbda24c0ac0635ebe819824
Juli Arnalot <juli.arnalot@gmail.com>
2024-09-25 12:38:11 +0200

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,13 @@ import type { NextRequest } from "next/server";
 import { jwtVerify } from "jose";
 
 export async function middleware(request: NextRequest) {
-  // Check the environment variable
   const maintenanceMode = process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "true";
   const allowedUsers = (process.env.NEXT_PUBLIC_ALLOWED_USERS || "").split(",");
   const token = request?.cookies.get("token");
 
-  if (!token) return NextResponse.redirect(new URL("/maintenance", request.url));
+  if (maintenanceMode && !token) return NextResponse.redirect(new URL("/maintenance", request.url));
+
+  if (!token) return NextResponse.next();
 
   const { payload } = await jwtVerify(
     token.value,
@@ -23,7 +24,6 @@ export async function middleware(request: NextRequest) {
 
   // If maintenance mode is enabled and the user is not already on the maintenance page
   if (maintenanceMode && !request.nextUrl.pathname.startsWith("/maintenance")) {
-    // Redirect to the maintenance page
     return NextResponse.redirect(new URL("/maintenance", request.url));
   }
 
