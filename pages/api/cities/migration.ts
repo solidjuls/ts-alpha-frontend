a8f4bdbd7d1d96810ed6a28bcd9f33217dbbd79d
Juli Arnalot <juli.arnalot@gmail.com>
2024-12-06 16:41:20 +0100

    added code to create city/timezones script

---
+++ pages/api/cities/migration.ts
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const csvParser = require('csv-parser');
+
+export default async function handler(req, res) {
+    try {
+        const updates = [];
+        const csvFilePath = path.join(__dirname, '../../../../../python/initial_data_seeding/Alpha migrating - CITIES.csv');
+        
+        fs.createReadStream(csvFilePath)
+        .pipe(csvParser({ headers: ['ID', 'city', 'city_ascii', 'lat', 'lng', 'fed', 'province', 'Timezone'] }))
+        .on('data', (row) => {
+          if (row.city !== 'city') {
+            const updateSQL = `UPDATE cities SET timeZoneId = '${row.Timezone}' WHERE id = ${row.ID};`;
+            updates.push(updateSQL);
+          }
+        })
+        .on('end', () => {
+          fs.writeFileSync('timezonesUpdate.sql', updates.join('\n'), 'utf-8');
+        });
+    } catch (e) {
+        console.error('Error with cities: ', e);
+    }
+  }
