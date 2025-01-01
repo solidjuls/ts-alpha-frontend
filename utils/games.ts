f200c656f127a195e8c9a98ee078f2cb1491c86b
Juli Arnalot <juli.arnalot@gmail.com>
2025-01-01 18:48:53 +0100

    fixed turn number on game id page

---
+++ utils/games.ts
@@ -0,0 +1,15 @@
+import { GameWinner } from "types/game.types";
+import { turns } from "utils/constants";
+
+export const getWinnerText = (gameWinner: GameWinner) => {
+  if (gameWinner === "1") {
+    return "USA";
+  } else if (gameWinner == "2") {
+    return "USSR";
+  }
+  return "TIE";
+};
+
+export const getTurnText = (turnNumber: number) => {
+  return turns.find((turn) => turn.value === String(turnNumber))?.text;
+};
