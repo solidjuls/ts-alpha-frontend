a92f09390b4eba1770cb6206ef8ccd7206a02994
Juli Arnalot <juli.arnalot@gmail.com>
2025-02-16 20:14:13 +0100

    fixed error on middleware jwp check (#80)

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,9 @@ import { jwtVerify } from "jose";
 
 export async function middleware(request: NextRequest) {
   const maintenanceMode = process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "true";
+
+  if (!maintenanceMode) return NextResponse.next();
+
   const allowedUsers = (process.env.NEXT_PUBLIC_ALLOWED_USERS || "").split(",");
   const token = request?.cookies.get("token");
 
@@ -11,13 +14,18 @@ export async function middleware(request: NextRequest) {
 
   if (!token) return NextResponse.next();
 
-  const { payload } = await jwtVerify(
-    token.value,
-    new TextEncoder().encode(process.env.TOKEN_SECRET),
-  );
+  try {
+    const { payload } = await jwtVerify(
+      token.value,
+      new TextEncoder().encode(process.env.TOKEN_SECRET),
+    );
 
-  if (allowedUsers.includes(payload.id)) {
-    return NextResponse.next();
+    if (allowedUsers.includes(payload.id)) {
+      return NextResponse.next();
+    }
+  } catch(error: unknown) {
+    console.log("token expired from middleware")
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   // If maintenance mode is enabled and the user is not already on the maintenance page
