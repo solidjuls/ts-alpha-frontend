36a0849b5c550b43544374da88cb387a4b0f9f59
Juli Arnalot <juli.arnalot@gmail.com>
2025-07-04 14:41:55 +0200

    schedule retrieval first iteration

--- pages/api/schedule/schedule.ts
+++ pages/api/schedule/schedule.ts
@@ -1,22 +1,22 @@
-import { NextApiRequest, NextApiResponse } from "next";
+// import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    switch (req.method) {
-        case "GET": {
-            const { uId, tId } = req.query;
-            // check if user already registered or not
-            // return all users registered for the tournament
-            return res.status(200).json(tournaments);
-        }
-      case "POST": {
-        const { uId, tId } = req.body;
-        // save a schedule
-        return res.status(200).json(tournaments);
-      }
-    }
-} catch (error) {
-    console.error("SCHEDULE API Error:", error);
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
-}
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   try {
+//     switch (req.method) {
+//         case "GET": {
+//             const { uId, tId } = req.query;
+//             // check if user already registered or not
+//             // return all users registered for the tournament
+//             return res.status(200).json(tournaments);
+//         }
+//       case "POST": {
+//         const { uId, tId } = req.body;
+//         // save a schedule
+//         return res.status(200).json(tournaments);
+//       }
+//     }
+// } catch (error) {
+//     console.error("SCHEDULE API Error:", error);
+//     return res.status(500).json({ error: "Internal Server Error" });
+//   }
+// }
