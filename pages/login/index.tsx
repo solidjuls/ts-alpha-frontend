ead56d4e2aa82e23a6098cbd2150dffd3bc5277a
Juli Arnalot <juli.arnalot@gmail.com>
2025-11-13 17:56:15 +0100

    remove compromised account id check

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -153,16 +153,6 @@ const LoginForm: React.FC = () => {
 const LoggedOutForm = () => {
   const { logout, name, id } = useSession();
 
-  useEffect(() => {
-    const ddd = async () => {
-      if (id === "2224") {
-        console.log("fuck off");
-        if (logout) await logout();
-      }
-    };
-    ddd()
-  }, []);
-
   return (
     <>
       <Head>
--- pages/submitform/index.tsx
+++ pages/submitform/index.tsx
@@ -317,14 +317,6 @@ export async function getServerSideProps({ req, res }: ServerType) {
     };
   }
 
-  if (payload?.id === "2224") {
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/login",
-      },
-    };
-  }
   return { props: { role: payload.role || null } };
 }
 
