c55aaad9cd90914b5b4093220dd0b50237364d3e
Juli Arnalot <juli.arnalot@gmail.com>
2026-01-07 19:30:41 +0100

    removed padding modifications

--- components/Homepage/Homepage.styled.ts
+++ components/Homepage/Homepage.styled.ts
@@ -89,12 +89,10 @@ export const PlayerCard = styled.div`
   border-radius: 12px;
   box-shadow: var(--shadow-soft);
 
-  padding: 4px 12px;
+  padding: 10px 12px;
 
   display: flex;
   flex-direction: column;
-
-  box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
 
   transition: transform 0.15s ease, box-shadow 0.15s ease, border-color 0.15s ease;
 
