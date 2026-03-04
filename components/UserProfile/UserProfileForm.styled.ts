a17a4920c3157e940e56247663caa6ce9e0f9f35
Juli Arnalot <juli.arnalot@gmail.com>
2026-03-04 19:28:40 +0100

    fixed userprofile width issues on mobile

--- components/UserProfile/UserProfileForm.styled.ts
+++ components/UserProfile/UserProfileForm.styled.ts
@@ -2,7 +2,6 @@ export const formStyles = {
   alignItems: "center",
   color: "var(--primary-text)",
   backgroundColor: "var(--bg-card)",
-  width: "640px",
   padding: "12px",
   alignSelf: "center",
   "@sm": {
